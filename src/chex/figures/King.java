1f4a3d3e5115c085aa2baaed757e0890b26419da
Leon Sell <sellleon@fim.uni-passau.de>
2015-07-30 01:20:04 +0200

    started working on king move validtion. not done on castling.

--- src/chex/figures/King.java
+++ src/chex/figures/King.java
@@ -5,13 +5,51 @@ import chex.Square;
 import chex.Vector2d;
 
 public class King extends Figure {
+    public static final int INVALID_KING_MOVE = 601;
+    public static final int OWN_PIECE_COLLISION_ERROR = 602;
+
+    private boolean hasMoved;
+
     public King(Player owner) {
+        hasMoved = false;
         owner.registerKing(this);
     }
 
     @Override
     public int validateMove(Player player, Vector2d from, Vector2d to, Square[][] board, int turnCount) {
-        return 0;
+        Vector2d moveVector = to.sub(from);
+
+        if (moveVector.abs().max() == 1) {
+            if (board[to.getY()][to.getX()].getPiece() != null
+                && board[to.getY()][to.getX()].getPiece().belongsTo(player)) {
+                return OWN_PIECE_COLLISION_ERROR;
+            } else {
+                if (!hasMoved) {
+                    hasMoved = true;
+                    player.setMovedKing(true);
+                }
+                return 0;
+            }
+        } else if (!hasMoved) {
+            //castling
+            if (moveVector.equals(new Vector2d(2, 0)) && !player.hasMovedRightRook()) {
+                //todo: impl
+
+                hasMoved = true;
+                player.setMovedKing(true);
+                return 0;
+            } else if (moveVector.equals(new Vector2d(-2 ,0)) && !player.hasMovedLeftRook()) {
+                //todo: impl
+
+                hasMoved = true;
+                player.setMovedKing(true);
+                return 0;
+            } else {
+                return INVALID_KING_MOVE;
+            }
+        } else {
+            return INVALID_KING_MOVE;
+        }
     }
 
     @Override
